556b68a1525f9d7ea1fda9b2c100ba39a857c332
poojan-bacancy <poojan.bhatt@bacancy.com>
2021-01-02 19:04:51 +0530

    issue : autofocus prop on textinput

--- screens/NumberScreen.js
+++ screens/NumberScreen.js
@@ -1,26 +1,43 @@
-import React , {useState,useEffect} from 'react';
+import React , {useState} from 'react';
 import { TextInput,ScrollView,TouchableWithoutFeedback,KeyboardAvoidingView,Keyboard,TouchableOpacity,View,StyleSheet,Image,Text, SafeAreaView} from 'react-native';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Flag from 'react-native-flags';
 
 const NumberScreen = props => {
 
+    const [mobileNumber , setMobileNumber] = useState();
+    const [error,setError] = useState('');
+
+    const changePhoneNumber = (number) => {
+        
+    }
+
     return  (
         <TouchableWithoutFeedback onPress={() => {
             Keyboard.dismiss();
         }}>
         <KeyboardAvoidingView style={{flex:1}}>
-                <View style={{marginVertical : 50 , marginHorizontal : 18}}><Text style={{fontFamily:'Gilroy-Medium' , fontWeight : '600', fontSize:26}}>Enter your Mobile Number</Text></View>
-                <View style={{marginVertical : 10 , marginHorizontal : 18}}><Text style={{fontFamily:'Gilroy-Medium' , fontWeight : '600', fontSize:16 , color : '#707070'}}>Mobile Number</Text></View>
-         
-            <View style={styles.inputBlock}>
-                <Flag
-                    code="IN"
-                    size={32}
-                />
-                <View style={styles.input}><TextInput keyboardType={'number-pad'}/></View>
+            <View style={styles.headerMobileNumber}>
+                <Text style={styles.headerMobileNumberText}>Enter your Mobile Number</Text>
             </View>
-            <View style={{ flex: 1 ,justifyContent :'flex-end' , alignItems : 'flex-end'}}>
+            <View style={styles.smallHeaderMobileNumber}>
+                <Text style={styles.smallHeaderMobileNumberText}>Mobile Number</Text>
+            </View>         
+            <View style={styles.inputBlock}>
+                <Flag code="IN" size={32}/>
+                <View style={{marginLeft : 10}} ><Text>+91</Text></View>
+                <View style={styles.input}>
+                    <TextInput
+                        value={mobileNumber} 
+                        maxLength={10}
+                        keyboardType={'number-pad'}
+                        onChangeText = {changePhoneNumber}
+                        autoFocus = {true}
+                    />
+                </View>
+            </View>
+    
+            <View style={styles.belowInputBlock}>
                 <View>
                     <TouchableOpacity style={styles.button} >
                         <Text style={styles.buttonText}>{'>'}</Text>
@@ -33,10 +50,24 @@ const NumberScreen = props => {
 }
 
 NumberScreen.navigationOptions = {
-    headerShown : null
+    headerTitle : ''
 }
 
 const styles = StyleSheet.create({
+    headerMobileNumber : {
+        marginVertical : 50 , 
+        marginHorizontal : 18
+    },
+    smallHeaderMobileNumber : {
+        marginVertical : 10 , 
+        marginHorizontal : 18
+    },
+    headerMobileNumberText : {
+        fontFamily:'Gilroy-Medium' , fontWeight : '600', fontSize:26
+    },
+    smallHeaderMobileNumberText : {
+        fontFamily:'Gilroy-Medium' , fontWeight : '600', fontSize:16 , color : '#707070'
+    },
     inputBlock : {
         flexDirection : 'row',
         alignItems : 'center',
@@ -48,6 +79,9 @@ const styles = StyleSheet.create({
     input:{
         flex: 1,
         marginLeft : 8,
+    },
+    belowInputBlock: {
+        flex: 1 ,justifyContent :'flex-end' , alignItems : 'flex-end'
     },
     button :{
         height : 62 , 
